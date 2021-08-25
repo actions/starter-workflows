40f3dbb3f561012f0c3fc23feb64c6b3784d9d9a
aparna-ravindra <aparna-ravindra@github.com>
2021-08-25 15:50:46 +0530

    validate categories script init

---
+++ script/validate-categories/validate.rb
@@ -0,0 +1,39 @@
+require 'linguist'
+require 'json'
+
+@languages = Linguist::Language.all.map(&:name)
+settings = JSON.parse(File.read('./settings.json'))
+folders = settings['folders']
+@allowed_categories = settings['allowed_categories']
+
+def validateCategories(categories)
+    categoryErrors = []
+    categories && categories.each do |category|
+        if ! @allowed_categories.include?(category) && !@languages.include?(category)
+            categoryErrors.push("unrecognised category #{category}")
+        end
+    end
+    return categoryErrors
+end
+
+result = []
+for folder in folders
+    files = Dir.entries(folder).select {|entry| File.file?(File.join(folder, entry)) && (File.extname(entry) == ".yaml" || File.extname(entry) == ".yml") }
+    for file in files
+        workflowId = File.basename(file,  File.extname(file))
+        errors = []
+        propertiesPath = folder + "/" + "properties/" + workflowId+".properties.json"
+        if(File.exist?(propertiesPath)) 
+            properties = JSON.parse(File.read(propertiesPath))
+            categoriesErrors = validateCategories(properties['categories'])
+            categoriesErrors.each { |e| errors.push(e) }
+        else
+            errors.push("properties file not found")
+        end
+        if errors.length > 0
+            result.push({"id" => workflowId, "errors" => errors})
+        end
+    end
+    puts result
+end
+
