382de8a69fd699b34709a33ecffbd80462544d18
Aparna Ravindra <82894348+aparna-ravindra@users.noreply.github.com>
2021-08-26 17:35:49 +0530

    Update validate.rb

--- script/validate-categories/validate.rb
+++ script/validate-categories/validate.rb
@@ -42,9 +42,10 @@ if result.length > 0
     generic_message = ":heavy_exclamation_mark: There are unrecognised categories found in the repo. Please note that using the right categories will result in better visibility and recommendation."
     header = "|Workflow Template Id|Unrecognised Categories|"
     table_alignment = "|:----:  |:----:  |"
-    rows = result.map { |r| "|#{r["id"]}|#{r["errors"]}|"}
     puts "::set-output name=generic_message:: #{generic_message}"
     puts "::set-output name=header:: #{header}"
     puts "::set-output name=table_alignment:: #{table_alignment}"
-    puts "::set-output name=unrecognised-categories:: #{rows}"
+    result.each do |r|
+        puts "::set-output name=unrecognised-categories-#{r["id"]}:: |#{r["id"]}|#{r["errors"]}|"
+    end
 end
