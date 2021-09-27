b58a4e21c6a523516a4c6d5d108a28281e9b7814
Nick Fyson <nickfyson@github.com>
2021-09-27 20:32:30 +0100

    start validating code-scanning workflows

--- script/validate-data/index.ts
+++ script/validate-data/index.ts
@@ -49,7 +49,7 @@ async function checkWorkflows(folders: string[]): Promise<WorkflowWithErrors[]> 
     });
 
     for (const e of dir) {
-      if (e.isFile()) {
+      if (e.isFile() && extname(e.name) === ".yml") {
         const fileType = basename(e.name, extname(e.name))
 
         const workflowFilePath = join(folder, e.name);
