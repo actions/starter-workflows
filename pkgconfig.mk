68085cfa47c6e36c5923943629fd30d1af139a26
Kornphan Muangbun <84589226+Kornphan@users.noreply.github.com>
2021-09-28 17:27:07 +0700

    Add files via upload

---
+++ pkgconfig.mk
@@ -0,0 +1,13 @@
+# Package version of Unicorn for Makefile.
+# To be used to generate unicorn.pc for pkg-config
+# Also used to generate python package version
+
+# version major & minor
+PKG_MAJOR = 1
+PKG_MINOR = 0
+
+# version bugfix level. Example: PKG_EXTRA = 1
+PKG_EXTRA = 3
+
+# version tag. Examples: rc1, b2, post1
+# PKG_TAG = rc6
