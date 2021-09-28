70655750b2798ee5171c044e10795b992e27ee6f
Nick Fyson <nickfyson@github.com>
2021-09-28 09:37:43 +0100

    check for yml and yaml extensions

--- script/validate-data/index.ts
+++ script/validate-data/index.ts
@@ -49,7 +49,7 @@ async function checkWorkflows(folders: string[]): Promise<WorkflowWithErrors[]> 
     });
 
     for (const e of dir) {
-      if (e.isFile() && extname(e.name) === ".yml") {
+      if (e.isFile() && [".yml", ".yaml"].includes(extname(e.name))) {
         const fileType = basename(e.name, extname(e.name))
 
         const workflowFilePath = join(folder, e.name);
